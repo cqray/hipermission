432fc4596df0a9c2672d6384a957b15f2cf92169
LeiJue <tipmail@qq,com>
2020-01-05 20:54:55 +0800

    commit

--- library/src/main/java/com/uguke/permission/SettingUtil.java
+++ library/src/main/java/com/uguke/permission/SettingUtil.java
@@ -22,7 +22,7 @@ public class SettingUtil {
     /** 小米 **/
     private static final String MANUFACTURER_XIAOMI = "Xiaomi";
     /** 索尼 **/
-    private static final String MANUFACTURER_openSony = "openSony";
+    private static final String MANUFACTURER_Sony = "openSony";
     /** OPPO **/
     private static final String MANUFACTURER_OPPO = "OPPO";
     /** LG **/
@@ -46,31 +46,32 @@ public class SettingUtil {
      * 跳转到相应品牌手机系统权限设置页，如果跳转不成功，则跳转到应用详情页
      * 这里需要改造成返回true或者false，应用详情页:true，应用权限页:false
      */
-    public static void openSetting(Activity act){
-        switch (Build.MANUFACTURER){
-            case MANUFACTURER_HUAWEI: openHuawei(act); break;
-            case MANUFACTURER_MEIZU: openMeizu(act); break;
-            case MANUFACTURER_XIAOMI: openXiaomi(act); break;
-            case MANUFACTURER_openSony: openSony(act); break;
-            case MANUFACTURER_OPPO: openOppo(act); break;
-            case MANUFACTURER_LG: openLG(act); break;
-            case MANUFACTURER_LETV: openLetv(act); break;
+    public static boolean openSetting(Activity act) {
+        switch (Build.MANUFACTURER) {
+            case MANUFACTURER_HUAWEI: return openHuawei(act);
+            case MANUFACTURER_MEIZU: return openMeizu(act);
+            case MANUFACTURER_XIAOMI: return openXiaomi(act);
+            case MANUFACTURER_Sony: return openSony(act);
+            case MANUFACTURER_OPPO: return openOppo(act);
+            case MANUFACTURER_LG: return openLG(act);
+            case MANUFACTURER_LETV: return openLetv(act);
             default:
                 try {
-                    //防止应用详情页也找不到，捕获异常后跳转到设置，这里跳转最好是两级，太多用户也会觉得麻烦，还不如不跳
+                    // 防止应用详情页也找不到，捕获异常后跳转到设置，
+                    // 这里跳转最好是两级，太多用户也会觉得麻烦，还不如不跳
                     openNativeSetting(act);
-//                    activity.startActivityForResult(getNativeSettingIntent(activity), PERMISSION_SETTING_FOR_RESULT);
                 } catch (Exception e) {
                     SystemConfig(act);
                 }
                 break;
         }
+        return false;
     }
 
     /**
      * 打开华为权限设置页
      */
-    private static void openHuawei(Activity act) {
+    private static boolean openHuawei(Activity act) {
         try {
             Intent intent = new Intent();
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -79,53 +80,50 @@ public class SettingUtil {
                     "com.huawei.permissionmanager.ui.MainActivity");
             intent.setComponent(comp);
             act.startActivityForResult(intent, PERMISSION_SETTING_FOR_RESULT);
-            isAppSettingOpen = false;
+            return true;
         } catch (Exception e) {
-            openNativeSetting(act);
+            return false;
         }
     }
 
     /**
      * 打开魅族权限设置页，测试时，点击无反应，具体原因不明
      */
-    private static void openMeizu(Activity act) {
+    private static boolean openMeizu(Activity act) {
         try {
             Intent intent = new Intent("com.meizu.safe.security.SHOW_APPSEC");
             intent.addCategory(Intent.CATEGORY_DEFAULT);
-            intent.putExtra("packageName", BuildConfig.APPLICATION_ID);
+            intent.putExtra("packageName", act.getPackageName());
             act.startActivity(intent);
-            isAppSettingOpen = false;
+            return true;
         } catch (Exception e) {
-            openNativeSetting(act);
-
+            return false;
         }
     }
 
     /**
      * 小米，功能正常
      */
-    private static void openXiaomi(Activity act) {
+    private static boolean openXiaomi(Activity act) {
         try {
             // MIUI 8 9
             Intent localIntent = new Intent("miui.intent.action.APP_PERM_EDITOR");
-            localIntent.setClassName("com.miui.securitycenter", "com.miui.permcenter.permissions.PermissionsEditorActivity");
+            localIntent.setClassName("com.miui.securitycenter",
+                    "com.miui.permcenter.permissions.PermissionsEditorActivity");
             localIntent.putExtra("extra_pkgname", act.getPackageName());
             act.startActivityForResult(localIntent, PERMISSION_SETTING_FOR_RESULT);
-            isAppSettingOpen = false;
+            return true;
         } catch (Exception e) {
             try {
                 // MIUI 5/6/7
                 Intent localIntent = new Intent("miui.intent.action.APP_PERM_EDITOR");
-                localIntent.setClassName("com.miui.securitycenter", "com.miui.permcenter.permissions.AppPermissionsEditorActivity");
+                localIntent.setClassName("com.miui.securitycenter",
+                        "com.miui.permcenter.permissions.AppPermissionsEditorActivity");
                 localIntent.putExtra("extra_pkgname", act.getPackageName());
                 act.startActivityForResult(localIntent, PERMISSION_SETTING_FOR_RESULT);
-                isAppSettingOpen = false;
+                return true;
             } catch (Exception e1) {
-                // 否则跳转到应用详情
-                openNativeSetting(act);
-//                activity.startActivityForResult(getNativeSettingIntent(activity), PERMISSION_SETTING_FOR_RESULT);
-                //这里有个问题，进入活动后需要再跳一级活动，就检测不到返回结果
-//                activity.startActivity(getNativeSettingIntent());
+                return false;
             }
         }
     }
@@ -133,89 +131,72 @@ public class SettingUtil {
     /**
      * 索尼，6.0以上的手机非常少，基本没看见
      */
-    private static void openSony(Activity act) {
+    private static boolean openSony(Activity act) {
         try {
             Intent intent = new Intent();
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.putExtra("packageName", BuildConfig.APPLICATION_ID);
-            ComponentName comp = new ComponentName("com.openSonymobile.cta", "com.openSonymobile.cta.SomcCTAMainActivity");
+            intent.putExtra("packageName", act.getPackageName());
+            ComponentName comp = new ComponentName("com.sonymobile.cta",
+                    "com.sonymobile.cta.SomcCTAMainActivity");
             intent.setComponent(comp);
             act.startActivity(intent);
-            isAppSettingOpen = false;
+            return true;
         } catch (Exception e) {
-            openNativeSetting(act);
-
+            return false;
         }
     }
 
     /**
      * 打开OPPO权限管理页
      */
-    private static void openOppo(Activity act) {
+    private static boolean openOppo(Activity act) {
         try {
             Intent intent = new Intent();
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.putExtra("packageName", BuildConfig.APPLICATION_ID);
+            intent.putExtra("packageName", act.getPackageName());
             ComponentName comp = new ComponentName("com.color.safecenter",
                     "com.color.safecenter.permission.PermissionManagerActivity");
             intent.setComponent(comp);
             act.startActivity(intent);
-            isAppSettingOpen = false;
+            return true;
         } catch (Exception e) {
-            openNativeSetting(act);
+            return false;
         }
     }
 
     /**
      * LG经过测试，正常使用
      */
-    private static void openLG(Activity act) {
+    private static boolean openLG(Activity act) {
         try {
             Intent intent = new Intent("android.intent.action.MAIN");
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.putExtra("packageName", BuildConfig.APPLICATION_ID);
+            intent.putExtra("packageName", act.getPackageName());
             ComponentName comp = new ComponentName("com.android.settings",
                     "com.android.settings.Settings$AccessLockSummaryActivity");
             intent.setComponent(comp);
             act.startActivity(intent);
-            isAppSettingOpen = false;
-        } catch (Exception e){
-            openNativeSetting(act);
-
+            return true;
+        } catch (Exception e) {
+            return false;
         }
     }
 
     /**
      * 乐视6.0以上很少，基本都可以忽略了，现在乐视手机不多
      */
-    private static void openLetv(Activity act) {
+    private static boolean openLetv(Activity act) {
         try {
             Intent intent = new Intent();
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.putExtra("packageName", BuildConfig.APPLICATION_ID);
-            ComponentName comp = new ComponentName("com.letv.android.letvsafe", "com.letv.android.letvsafe.PermissionAndApps");
+            intent.putExtra("packageName", act.getPackageName());
+            ComponentName comp = new ComponentName("com.letv.android.letvsafe",
+                    "com.letv.android.letvsafe.PermissionAndApps");
             intent.setComponent(comp);
             act.startActivity(intent);
-            isAppSettingOpen = false;
+            return true;
         } catch (Exception e) {
-            openNativeSetting(act);
-        }
-    }
-
-    /**
-     * 360只能打开到自带安全软件
-     */
-    public static void _360(Activity act) {
-        try {
-            Intent intent = new Intent("android.intent.action.MAIN");
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.putExtra("packageName", BuildConfig.APPLICATION_ID);
-            ComponentName comp = new ComponentName("com.qihoo360.mobilesafe", "com.qihoo360.mobilesafe.ui.index.AppEnterActivity");
-            intent.setComponent(comp);
-            act.startActivity(intent);
-        } catch (Exception e) {
-            openNativeSetting(act);
-
+            return false;
         }
     }
 
@@ -230,7 +211,7 @@ public class SettingUtil {
     /**
      * 打开原生应用详情页
      */
-    private static void openNativeSetting(Activity act){
+    private static void openNativeSetting(Activity act) {
         Intent intent = new Intent();
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
@@ -242,6 +223,5 @@ public class SettingUtil {
             intent.putExtra("com.android.settings.ApplicationPkgName", act.getPackageName());
         }
         act.startActivityForResult(intent, PERMISSION_SETTING_FOR_RESULT);
-        isAppSettingOpen = true;
     }
 }
